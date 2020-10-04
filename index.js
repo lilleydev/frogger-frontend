eeb251a91c4b040abdb72d0ced75b9260ddf3cda
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-03 18:52:43 -0700

    add class, constructor, post, and delete button

--- index.js
+++ index.js
@@ -14,12 +14,72 @@ function renderPost(post) {
   const postEl = document.createElement("p");
   const postP = document.createElement("P");
   const postH2 = document.createElement("h2");
+  const deleteBtn = document.createElement("button");
 
   postH2.innerText = `${post.title}`;
   postH2.id = `${post.id}`;
   postEl.innerText = `${post.content}`;
   postP.innerText = `${post.mood}`;
+  deleteBtn.innerText = "delete";
   postContainer.appendChild(postH2);
   postContainer.appendChild(postEl);
   postContainer.appendChild(postP);
+  postContainer.appendChild(deleteBtn);
 }
+
+class Post {
+  constructor(title, content, mood) {
+    (this.title = title), (this.content = content), (this.mood = mood);
+  }
+
+  persist() {
+    fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        post: {
+          title: this.title,
+          content: this.content,
+          mood: this.mood,
+          user: "1",
+        },
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        renderPost(data);
+      });
+  }
+
+  // destroyPost() {
+  //   fetch(`http://localhost:3000/posts/${post.id}`, {
+  //     method: "DELETE",
+
+  //     }).then(() => console.log(delete),
+
+  // }
+}
+
+function submitPost(post) {
+  post.persist();
+}
+
+const postForm = document.getElementById("newPost");
+postForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // debugger;
+  const post = new Post({
+    attributes: {
+      title: postForm.title.value,
+      content: postForm.content.value,
+      mood: postForm.mood.value,
+    },
+  });
+  submitPost(post);
+  postForm.reset();
+});
+
+// delete button
+//edit button
